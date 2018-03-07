e01e8d7c726fb24d62e97d3f7eaf52de06f0dc1d
Sushant Paudel <sushant@yarsa.io>
2018-03-07 20:37:28 +0545

    BayesianClassifier Class functions added

--- src/classification/BayesianAlgorithm.java
+++ src/classification/BayesianAlgorithm.java
@@ -2,12 +2,24 @@ package classification;
 
 import models.DataModelWeather;
 
+import java.util.ArrayList;
+
 public class BayesianAlgorithm {
     private static DataModelWeather[] dataSet = DataModelWeather.generateDataSet();
 
     public static void main(String[] args) {
-        for (DataModelWeather e : dataSet) {
+        ArrayList<Integer> counter = new ArrayList<>();
+        ArrayList<String> dataWeather = new ArrayList<>();
+        ArrayList<String> dataPlay = new ArrayList<>();
+        int countDataWeather = 0;
+        int countPlay = 0;
+        for (DataModelWeather dataModelWeather : dataSet) {
+            int countP;
+            for (int i = 0; i < countDataWeather; i++) {
+                if (dataModelWeather.getWeather().equals(dataWeather.get(i))) {
 
+                }
+            }
         }
     }
 }
